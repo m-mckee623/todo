a460b4afaaa019f6d46baef396f218fcbe40fc2a
Matthew <matthew.mckee.2018@gmail.com>
2024-10-14 17:48:02 +0200

    Added routeguard logic and cleaned up a bit of html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {ErrorComponent} from './error/error.component';
 import {ListTodosComponent} from './list-todos/list-todos.component';
 import {LogoutComponent} from './logout/logout.component';
+import {RouteGuardService} from './service/route-guard.service';
 
 export const routes: Routes = [
   // Nothing entered, take to the default which is the login component
@@ -11,8 +12,8 @@ export const routes: Routes = [
   // Take to the specified components below
   //E.g. baseURL/login
   { path:'login', component: LoginComponent},
-  { path:'logout', component: LogoutComponent},
-  { path:'welcome/:name', component: WelcomeComponent},
-  { path:'todos', component: ListTodosComponent},
+  { path:'logout', component: LogoutComponent, canActivate:[RouteGuardService]},
+  { path:'welcome/:name', component: WelcomeComponent, canActivate:[RouteGuardService]},
+  { path:'todos', component: ListTodosComponent, canActivate:[RouteGuardService]},
   { path:'**', component: ErrorComponent}
 ];
--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  GuardResult,
+  MaybeAsync,
+  Router,
+  RouterStateSnapshot
+} from '@angular/router';
+import {HardcodedAuthenticationService} from './hardcoded-authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService {
+export class RouteGuardService implements CanActivate{
 
-  constructor() { }
+
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService,
+              private router: Router,) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+   if(this.hardcodedAuthenticationService.isUserLoggedIn())
+    return true;
+   else {
+     this.router.navigate(['login'])
+     return false;
+   }
+  }
 }
