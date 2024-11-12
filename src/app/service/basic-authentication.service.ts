09dd23a7218b4fd7498e34ec69b092cb7f54cff2
Matthew <matthew.mckee.2018@gmail.com>
2024-11-12 19:58:37 +0100

    1. Added basic-authetication_service£

---
+++ src/app/service/basic-authentication.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+  constructor(private http: HttpClient) { }
+
+  authenticate(username : string, password : String) {
+    if(username === "username" && password === "password"){
+      sessionStorage.setItem('authenticateUser', username)
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  executeBasicAuthenticationService(username:string, password:string){
+
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    })
+
+    return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`,
+      {headers});
+  }
+
+  isUserLoggedIn(){
+    let user: string | null = sessionStorage.getItem('authenticateUser')
+    return !(user === null)
+  }
+
+  logout(){
+    sessionStorage.removeItem('authenticateUser')
+  }
+}
+
+
+export class AuthenticationBean{
+  constructor(public message:string){
+
+  }
+}
